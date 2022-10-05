65d85792a888928674e2efcd399332eb077f6df1
Paweł <pawel_gnat@o2.pl>
2022-10-05 20:25:44 +0200

    added comments to functions, completed media queries

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@
 const toDoList = document.querySelector('.tasks-box')
 const createNewTaskInput = document.querySelector('.input-container__input')
 const inputError = document.querySelector('.input-error')
-const currentTasksNumber = document.querySelector('.button-container__text')
-const clearCompletedTasksBtn = document.querySelector('.button-container__clear-btn--btn')
+
+const taskButtons = [
+	...document.querySelectorAll('.button-container button'),
+	...document.querySelectorAll('.button-container2 button'),
+]
 
 const tasks = document.getElementsByClassName('task')
 const emptyStateContainer = document.getElementsByClassName('empty-state')
@@ -13,10 +16,11 @@ let taskId = 0
 let inputId = 0
 
 function emptyState() {
+	// display instructions how to use and application if there are no tasks created
 	const emptyState = document.createElement('div')
 	emptyState.className = 'empty-state'
 	emptyState.innerHTML = `
-		<h2 class="empty-state__heading">Please create new task</h2>
+		<h2 class="empty-state__heading">Please create a new task</h2>
 		<div class="empty-state__option">
 			<span class="empty-state__option--circle"></span>
 			<p class="empty-state__option--text">Select circle or text to mark task as completed</p>
@@ -30,6 +34,7 @@ function emptyState() {
 }
 
 function handleState() {
+	// append empty state function
 	setTimeout(() => {
 		if (tasks.length === 0) {
 			toDoList.insertBefore(emptyState(), toDoList.firstChild)
@@ -38,7 +43,8 @@ function handleState() {
 }
 
 const createNewTask = () => {
-	let emptyspace = /\s/
+	// function creates a new task
+	let emptyspace = /^\s*$/
 	let inputValue = createNewTaskInput.value
 
 	if (!inputValue.match(emptyspace) && inputValue !== '') {
@@ -74,6 +80,7 @@ const createNewTask = () => {
 }
 
 const displayNewTask = event => {
+	// funciton display a created task if input value is not empty
 	if (event.key == 'Enter') {
 		createNewTask()
 
@@ -88,6 +95,7 @@ const displayNewTask = event => {
 }
 
 const handleTask = e => {
+	// function search for a button inside tasks container and delete/set a complete them on click
 	if (e.target.closest('button')) {
 		deleteTask(e)
 		handleState()
@@ -100,6 +108,7 @@ const handleTask = e => {
 }
 
 function deleteTask(e) {
+	// delete task function
 	let deleteSelectedTask = e.target.closest('li')
 	deleteSelectedTask.classList.add('task-remove')
 
@@ -110,6 +119,7 @@ function deleteTask(e) {
 }
 
 function setTaskAsCompleted(e) {
+	// set as completed function
 	const completeTask = e.target.closest('li')
 	const completeTaskImg = completeTask.getElementsByTagName('span')[0]
 
@@ -118,6 +128,8 @@ function setTaskAsCompleted(e) {
 }
 
 function countTasks() {
+	// displays information about items left (left bottom corner)
+	const currentTasksNumber = document.querySelector('.button-container__text')
 	let tasksArray = [...tasks]
 	let activeArray = []
 
@@ -131,6 +143,7 @@ function countTasks() {
 }
 
 function deleteCompletedTasks() {
+	// delete completed task function
 	let tasksArray = [...tasks]
 	let completedArray = []
 
@@ -147,17 +160,41 @@ function deleteCompletedTasks() {
 	})
 }
 
+// const handleTaskButtons = e => {
+// 	if (e.target.closest('button')) {
+// 		deleteTask(e)
+// 		handleState()
+// 	}
+// }
+
+taskButtons.forEach(btn => {
+	// action on my task buttons
+	btn.addEventListener('click', () => {
+		if (btn.dataset.status == 'all') {
+			btn.classList.add('active-btn')
+		}
+
+		if (btn.dataset.status == 'active') {
+			btn.classList.add('active-btn')
+		}
+
+		if (btn.dataset.status == 'completed') {
+			btn.classList.add('active-btn')
+		}
+
+		if (btn.dataset.status == 'clear') {
+			deleteCompletedTasks()
+			handleState()
+		}
+	})
+})
+
 createNewTaskInput.addEventListener('keyup', displayNewTask)
 toDoList.addEventListener('click', handleTask)
-clearCompletedTasksBtn.addEventListener('click', deleteCompletedTasks)
 handleState()
 countTasks()
 
-// completed btn image and gradient
-// media queries
 // local storage
-// all active completed btny
 // drag and drop
 // color switcher
 // active state na buttonach dolnych
-// type module js
